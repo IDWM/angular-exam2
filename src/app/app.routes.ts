67c499b98a62f6d92e48204c16c362d956ab7397
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-12-08 15:45:54 -0300

    feat: add user management routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './pages/user-list/user-list.component';
+import { CreateUserComponent } from './pages/create-user/create-user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'user',
+    component: UserListComponent,
+  },
+  {
+    path: 'user/create',
+    component: CreateUserComponent,
+  },
+];
